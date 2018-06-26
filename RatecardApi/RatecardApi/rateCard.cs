058c83e51075b6957d57842bcc5a2c7536afd64e
Asha Polamarasetty <asha.polamarasetty@valuemomentum.biz>
2018-06-26 19:11:38 +0530

    Intial commit

---
+++ RatecardApi/RatecardApi/rateCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatecardApi
+{
+    public class RateCard
+    {
+        public List<object> OfferTerms { get; set; }
+        public List<Meter> Meters { get; set; }
+        public string Currency { get; set; }
+        public string Locale { get; set; }
+        public bool IsTaxIncluded { get; set; }
+    }
+}
